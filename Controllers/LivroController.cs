01fe975abe0c1e452a405ab233c1ce408c17b013
DarioNet0 <dariooliveirasneto@gmail.com>
2024-06-13 14:33:56 -0300

    Add project files.

---
+++ Controllers/LivroController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MinhaPrimeiraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LivroController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public IActionResult criarLivro([FromBody] Livro livro)
+        {
+            return Created();
+        }
+        [HttpGet]
+        [Route("{id}/{nome}")]
+        [ProducesResponseType(typeof(Livro), StatusCodes.Status200OK)]
+        public IActionResult consultarLivros(int id, string nome)
+        {
+            Livro livro = new Livro();
+            return Ok(livro);
+        }
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult atualizarLivro([FromBody] Livro livro)
+        {
+            return NoContent();
+        }
+        [HttpDelete]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult apagarLivro([FromBody] int id)
+        {
+            return NoContent();
+        }
+    }
+}
---
+++ Livro.cs
@@ -0,0 +1,12 @@
+﻿namespace MinhaPrimeiraAPI
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Autor { get; set; }
+        public string? Genero { get; set; }
+        public double? Preco { get; set; }
+        public int? QtdEstoque { get; set; }
+    }
+}
